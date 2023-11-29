784677264b6cb74a48477acc7ec97267b0b0efb3
watching-dev <coderefactoring90@gmail.com>
2023-11-30 01:06:23 +0900

    [feat] INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS 로 수정

--- Backend_Server/config/settings.py
+++ Backend_Server/config/settings.py
@@ -30,15 +30,21 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+CUSTOM_APPS = [
+'about.apps.AboutConfig',
+]
+
+
+SYSTEM_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'about.apps.AboutConfig',
 ]
+
+INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
